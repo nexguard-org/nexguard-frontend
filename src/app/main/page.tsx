7142b60d3db2157f95e907f34218846c23e3e125
Memz-idk <arthurdefigueiredo@hotmail.com>
2025-11-13 23:08:15 -0300

    Fix cache issue

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -5,6 +5,7 @@ import MainForm from "@/components/main/MainForm";
 async function getSourceSelectProps() {
     const values: {[key: string]: string} = await (await fetch(`http://${process.env.API_ADDRESS}/sources/list` as string, {
         method: "GET",
+        cache: "no-store"
     })).json();
     return {values: values, selected: Object.entries(values).at(0)?.[0]} as SelectOptions;
 }
